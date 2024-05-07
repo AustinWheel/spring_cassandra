274b01cae0b6a294c01fef04431d777f4b34e0a0
austinwheel <austinwheeler1112@gmail.com>
2024-05-07 03:18:54 -0400

    Add Vet table mapping

---
+++ src/main/java/com/example/gsaccessingdatacassandra/GsAccessingDataCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsaccessingdatacassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAccessingDataCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsAccessingDataCassandraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gsaccessingdatacassandra/GsAccessingDataCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsaccessingdatacassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccessingDataCassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
